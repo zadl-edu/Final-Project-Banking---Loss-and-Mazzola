515106706fe770d614bc7d34333b9f24bc5056e7
JMazzola <josephina.mazzola@mymail.champlain.edu>
2023-03-23 15:28:24 -0400

    Added comments for requirements

--- Final Project Banking - Loss and Mazzola/Source Files/Main.cpp
+++ Final Project Banking - Loss and Mazzola/Source Files/Main.cpp
@@ -9,3 +9,36 @@ int main()
 	ifstream input;
 	return 0;
 }
+
+/*
+1. Add a new account
+2. Delete an existing account
+3. Update information on an existing account3
+4. Search account information4
+5. Deposit money into an account
+6. Withdraw money from an account
+7. Check balance on an account
+
+You must incorporate the following components into your code:
+1. Constance
+2. Decision (if-statement and switch-statement)
+3. Repetition (for-loop, while-loop and do-loop)
+4. File manipulation
+5. IO manipulation
+6. Function
+7. Array
+
+Additional Requirements
+1. Your program style must comply with the Programming Standard.
+2. Your program must be compliable and executable according to the specification.
+3. You must include header information in your source code.
+4. All data files must be stored in a directory named data.
+5. All inputs must be validated.
+6. All dollar amounts must be display in 2-decimal places.
+7. All function must have the pre, post and purpose statements. Also, each function must
+bear the name of the author, if you are working with a partner.
+8. If any of the data file is missing, your program must not terminate. For example, if
+tellers.dat is not available, your system must continue to run. The consequence of
+missing this file is that no one can log in.
+9. You must submit a softcopy on Canvas
+*/
